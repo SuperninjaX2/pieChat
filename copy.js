8f9a9562b6677a31da75952267c7f7dd1121dc82
SuperninjaX2 <lunguj665@gmail.com>
2024-02-24 18:01:25 +0000

    rsync

--- copy.js
+++ copy.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-const sourceDir = '/storage/internal';
-const destinationDir = '/storage/code';
+const sourceDir = '/storage/internal/pieChat';
+const destinationDir = '/storage/code/pieChat';
 
 // Check if source directory exists
 if (!fs.existsSync(sourceDir)) {
@@ -17,12 +17,12 @@ if (!fs.existsSync(destinationDir)) {
 }
 
 try {
-    // Copy files from source directory to destination directory
-    execSync(`rsync -av ${sourceDir}/ ${destinationDir}`);
+    // Copy files from source directory to destination directory excluding node_modules
+    execSync(`rsync -av --exclude='node_modules' ${sourceDir}/ ${destinationDir}`);
     console.log('Files copied from source to destination successfully.');
 
-    // Copy files from destination directory to source directory
-    execSync(`rsync -av ${destinationDir}/ ${sourceDir}`);
+    // Copy files from destination directory to source directory excluding node_modules
+    execSync(`rsync -av --exclude='node_modules' ${destinationDir}/ ${sourceDir}`);
     console.log('Files copied from destination to source successfully.');
 } catch (err) {
     console.error('Error copying files:', err);
